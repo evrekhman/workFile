9bc41ef0842a97b5bc4d69e613eafe11745a2030
Пользователь <evrekhman@gmail.com>
2018-01-25 03:38:55 +0600

    first commit

---
+++ ab.php
@@ -0,0 +1,54 @@
+﻿<form enctype="multipart/form-data" method="POST">
+    
+    <input type="text" name="userfile">
+    <input type="submit" value="Send">
+</form>
+
+<?
+    
+    function readdFile($nameFile,$keyValue){
+        $handle = file_get_contents($nameFile.".txt");
+        $arra = array();
+        $ex = explode("x0A", $handle);
+
+        foreach ($ex as $key => $value) {
+            
+            $ex1 = explode("\\t", $value);        
+
+            foreach ($ex1 as $k => $v) {
+                
+                if($k==0){
+                    $kkk = $v;
+                }
+                if($k==1){
+                    $vvv = $v;
+                }
+                if(isset($vvv))
+                $arra[$kkk]=$vvv;
+
+            }
+
+        }
+        
+        
+
+        if($new = preg_grep("#".$keyValue."#", array_keys($arra))){
+           
+            foreach ($new as $key => $value) {
+                # code...
+                if($keyValue)
+                print_r($arra[$value]);
+            }
+            
+        }
+        
+
+      
+    }
+    
+    if(isset($_POST['userfile'])){
+        $text = $_POST['userfile'];
+        readdFile("ara",$text);
+    }
+
+?>
